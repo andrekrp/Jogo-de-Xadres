122a00c64a17ff3ff5fd1b29e2f32c988e533172
andrekrp <andrekrp@gmail.com>
2020-07-25 16:31:18 -0300

    Criando a classe pocição

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace JogoDeXadrez
 {
@@ -6,7 +7,11 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
